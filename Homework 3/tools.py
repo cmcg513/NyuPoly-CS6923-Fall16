d5100201def77a0d1fbd4aac8ba668c12706ba79
Casey McGinley <cmcg513@gmail.com>
2016-12-05 03:33:46 -0800

    modified code for part 1 section A

--- Homework 3/tools.py
+++ Homework 3/tools.py
@@ -1,7 +1,20 @@
+# Revised copy of tools.py script originally provided by Prof. Gustavo Sandoval
+#
+# Editor: Casey McGinley <cmm771@nyu.edu>
+# Original file: https://github.com/GusSand/NyuPoly-CS6923-Fall16/blob/master/Homework%203/tools.py
+# 
+# New functions added by the aforementioned editor are prefaced by the 
+# following: cmm771 Addition
+# Original functions edited by the aforementioned editor are prefaced by the 
+# following: cmm771 Edit
+
 from collections import defaultdict
 import re
 import math
 from unidecode import unidecode
+
+# The default list of words to skip when counting
+SKIP_WORDS_DEFAULT = ['python','data','with']
 
 spchars = re.compile('\`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\_|\+|\=|\\|\||\{|\[|\]|\}|\:|\;|\'|\"|\<|\,|\>|\?|\/|\.|\-')
 
@@ -19,25 +32,40 @@ def make_text_parsable(text):
     return(text)
 
 #
-# Tokenize by whitespace. Use the defaultdict(int) whichsets the default 
-# factory to int which makes it  the default dict useful for counting. 
+# cmm771 Edit
 #
-def count_words(text, wc=None):
+# Tokenize by whitespace. Use the defaultdict(int) whichsets the default 
+# factory to int which makes it  the default dict useful for counting.
+# 
+# Take list of words to skip when counting; also skips the empty string '' 
+#
+def count_words(text, wc=None, skip_words=None):
     if wc == None:
         wc = defaultdict(int)
+    if skip_words == None:
+        skip_words = []
     tokens = text.split(" ")
     for t in tokens:
-        wc[t] += 1  
+        if (t != '') and (t not in skip_words):
+            wc[t] += 1  
     return(wc)
 
 #
+# cmm771 Edit
+# 
 # Main function. Opens the file and calls helper functions to parse
 # Returns the sorted word count
+# 
+# Calls get_stopwords and combines the result with the skip_words list; this is
+# then passed to count_words in addition to the text and word count list
 #
-def extract_info(filename):
+def extract_info(filename, skip_words_manual=SKIP_WORDS_DEFAULT):
     import json
     wc = defaultdict(int)
     df = defaultdict(set)
+    skip_words = get_stopwords()
+    for word in skip_words_manual:
+        skip_words.add(word)
     count = 0
     with open(filename) as fin:
         for line in fin:
@@ -45,9 +73,26 @@ def extract_info(filename):
             current = json.loads(line)
             text = make_text_parsable(current["abstract"] + " " + \
                 current["description"] + " " + current["title"])
-            wc = count_words(text, wc)
+            wc = count_words(text, wc=wc, skip_words=skip_words)
     
 
     sorted_wc = sorted(wc.items(), key=lambda x: x[1], reverse=True)
     
     return sorted_wc
+
+# 
+# cmm771 Addition
+# 
+# Reads in the list of stopwords from a file (filename given as param; defaults
+# to stopwords.txt)
+# Returns stopwords as set
+#
+def get_stopwords(filename='stopwords.txt'):
+    stopwords = set()
+    with open(filename) as sword_file:
+        for line in sword_file:
+            line = line.strip()
+            if line == '':
+                continue
+            stopwords.add(line)
+    return stopwords
